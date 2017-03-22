6d75e7697d91f80c20b69c82ec46a59064b5c74f
mx1700 <mx1700@gmail.com>
2017-03-22 16:04:24 +0800

    完成简单的静态文件处理功能

--- src/main/kotlin/me/mx1700/koalin/App.kt
+++ src/main/kotlin/me/mx1700/koalin/App.kt
@@ -1,10 +1,13 @@
 package me.mx1700.koalin
 
+import me.mx1700.koalin.jetty.Resource
 import me.mx1700.koalin.jetty.listen
 
 fun main(args: Array<String>) {
-
     val app = Server()
+
+    app.use(Resource())
+
     app.use {
         response.body = "hello world! 你好，世界！"
         response.status = 200
@@ -23,17 +26,12 @@ ${request.queryString}
 ${request.secure}
 ${request.type}
 ${request.url}
+${request.href}
 """)
     }
 
     app.use {
         response.body += " next!"
-    }
-
-    app.onException { e ->
-        e.printStackTrace()
-        response.status = 500
-        response.body = "内部错误"
     }
 
     app.listen(9000)
---
+++ src/main/kotlin/me/mx1700/koalin/jetty/Resource.kt
@@ -0,0 +1,35 @@
+package me.mx1700.koalin.jetty
+
+import me.mx1700.koalin.Context
+import me.mx1700.koalin.Middleware
+import org.eclipse.jetty.http.MimeTypes
+import org.eclipse.jetty.server.ResourceContentFactory
+import org.eclipse.jetty.server.ResourceService
+import org.eclipse.jetty.util.resource.Resource
+import org.eclipse.jetty.util.resource.ResourceFactory
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class Resource : Middleware {
+    val _mimeTypes = MimeTypes()
+    val _resourceService = object: ResourceService() {
+        override fun notFound(request: HttpServletRequest?, response: HttpServletResponse?) {
+            //未找到文件则不处理
+        }
+    }
+
+    init {
+        _resourceService.contentFactory = ResourceContentFactory(ResourceFactory { path ->
+            Resource.newResource(path)
+        }, _mimeTypes, _resourceService.precompressedFormats)
+    }
+    override fun invoke(ctx: Context) {
+        val request = ctx.req
+        val response = ctx.res
+        _resourceService.doGet(request, response)
+        if (!response.isCommitted) {
+            ctx.next()
+        }
+    }
+
+}
